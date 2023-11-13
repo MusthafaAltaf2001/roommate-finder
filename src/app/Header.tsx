80ea5cc7e96c9e08734414cd056701efed4ee427
MusthafaAltaf2001 <musthafaaltaf2001@gmail.com>
2023-11-13 19:41:27 +0800

    Created header

---
+++ src/app/Header.tsx
@@ -0,0 +1,82 @@
+"use client"
+
+import React, { useState, useRef } from "react";
+
+const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const navRef = useRef()
+
+    const navBarToggle = () => {
+        setIsOpen(!isOpen)
+        // navRef.current.classList.toggle("hidden")
+    }
+
+    const scrollToHome = () => {
+        const element = document.getElementById("home")
+        element.scrollIntoView({ behavior: 'smooth' })
+    }
+
+    const scrollToAbout = () => {
+        const element = document.getElementById("about")
+        element.scrollIntoView({ behavior: 'smooth' })
+    }
+
+    const scrollToProjects = () => {
+        const element = document.getElementById("projects")
+        element.scrollIntoView({ behavior: 'smooth' })
+    }
+
+    const scrollToContact = () => {
+        const element = document.getElementById("footer")
+        element.scrollIntoView({ behavior: 'smooth' })
+    }
+
+    const genericHamburgerLine = `h-0.5 w-6 my-0.5 rounded-full bg-[#FFFFFF] transition ease transform duration-300 z-200`;
+
+    return (
+        <header id="header" className="sticky top-0 z-40">
+            <div className="bg-[#F8F8FF] flex h-16 md:px-[150px] xl:px-[450px] justify-center">
+                <div className="flex flex-row justify-center items-center">
+                    <div className="block max-md:hidden">
+                        <div className="flex items-center justify-center">
+                            <button onClick={scrollToHome} className="header-right-text">HOME</button>
+                            <button onClick={scrollToAbout} className="header-right-text">ABOUT</button>
+                            <button onClick={scrollToProjects} className="header-right-text">PROJECTS</button>
+                            <button onClick={scrollToContact} className="header-right-text">CONTACT</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div ref={navRef} className={`absolute left-0 top-0 bg-[#111827] ease-in-out duration-500 ${isOpen ? "translate-x-0 " : "-translate-x-full"}`}>
+                <div className="flex flex-col items-center justify-center pt-10 h-screen w-screen">
+                    <span onClick={() => { navBarToggle(); scrollToHome(); }} className="header-drawer-text" href="/#">HOME</span>
+                    <span onClick={() => { navBarToggle(); scrollToAbout(); }} className="header-drawer-text">ABOUT</span>
+                    <span onClick={() => { navBarToggle(); scrollToProjects(); }} className="header-drawer-text">PROJECTS</span>
+                    <span onClick={() => { navBarToggle(); scrollToContact(); }} className="header-drawer-text" href="/#">CONTACT</span>
+                </div>
+            </div>
+            <div className="z-200 absolute top-2 left-2 hidden max-md:block">
+                <button
+                    className="flex flex-col h-12 w-12 rounded justify-center items-center group"
+                    onClick={navBarToggle}
+                >
+                    <div
+                        className={`${genericHamburgerLine} ${isOpen
+                            ? "rotate-45 translate-y-1.5 opacity-50 group-hover:opacity-100"
+                            : "opacity-50 group-hover:opacity-100"
+                            }`}
+                    />
+                    <div className={`${genericHamburgerLine} ${isOpen ? "opacity-0" : "opacity-50 group-hover:opacity-100"}`} />
+                    <div
+                        className={`${genericHamburgerLine} ${isOpen
+                            ? "-rotate-45 -translate-y-1.5 opacity-50 group-hover:opacity-100"
+                            : "opacity-50 group-hover:opacity-100"
+                            }`}
+                    />
+                </button>
+            </div>
+        </header>
+    );
+}
+
+export default Header
