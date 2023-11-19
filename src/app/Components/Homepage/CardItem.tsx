504aa3e478f3d16ad0a610232ccedbc74335fda1
MusthafaAltaf2001 <musthafaaltaf2001@gmail.com>
2023-11-19 19:19:40 +0800

    Added CardList and CardItem

--- src/app/Components/Homepage/CardItem.tsx
+++ src/app/Components/Homepage/CardItem.tsx
@@ -5,13 +5,13 @@ import Image from 'next/image'
 const CardItem = () => {
     return (
         <div className='flex flex-col basis-auto border-0 rounded-xl'>
-            <Image src={house_img} alt="Image" className='border-0 rounded-t-xl object-fit w-full h-full' />
-            <div className='flex flex-col p-7 bg-[#ECEEFF] rounded-b-xl'>
+            <Image src={house_img} alt="Image" className='border-0 rounded-t-xl w-[400px] h-[250px] object-cover' />
+            <div className='flex flex-col p-5 bg-[#ECEEFF] rounded-b-xl'>
                 <div className='flex items-baseline'>
-                    <span className='text-blue font-bold text-[35px]'>$300</span>
+                    <span className='text-blue font-bold text-[30px]'>$300</span>
                     <span className='text-[#8A8DBA]'>/month</span>
                 </div>
-                <span className='text-black font-bold text-[45px]'>Subang Jaya</span>
+                <span className='text-black font-bold text-[35px]'>Subang Jaya</span>
                 <span className='text-[#8A8DBA]'>7, Jalan PJS 11/7, Bandar Sunway</span>
             </div>
         </div>
--- src/app/Components/Homepage/CardList.tsx
+++ src/app/Components/Homepage/CardList.tsx
@@ -3,7 +3,7 @@ import CardItem from './CardItem'
 
 const CardList = () => {
     return (
-        <div className='m-2 grid grid-cols-1 gap-10'>
+        <div className='m-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 place-items-center'>
             <CardItem />
             <CardItem />
             <CardItem />
