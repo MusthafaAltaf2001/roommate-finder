de2755e0587a2675af664fc08a5dd095dc29b444
MusthafaAltaf2001 <musthafaaltaf2001@gmail.com>
2023-12-09 01:29:22 +0800

    Removed userCount test function in Store.js

--- src/app/Store.ts
+++ src/app/Store.ts
@@ -3,14 +3,11 @@ import { devtools } from "zustand/middleware";
 
 interface UserState {
   user: Object;
-  count: number;
 }
 
 const useUserStore = create<UserState>()(
   devtools((set) => ({
     user: {},
-    count: 0,
-    userCount: (num: number) => set((state) => ({ count: num })),
     setUser: (user: any) => set((state) => ({ user: user })),
   })),
 );
