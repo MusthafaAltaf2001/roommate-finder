a919453b68f1f231d0b16c707d1be0e6fb9f4b35
MusthafaAltaf2001 <musthafaaltaf2001@gmail.com>
2023-12-12 13:11:58 +0800

    test prod change

--- src/app/Pages/Homepage.tsx
+++ src/app/Pages/Homepage.tsx
@@ -7,6 +7,7 @@ import CardList from "../Components/Homepage/CardList";
 
 const Homepage = () => {
   console.log(process.env.NODE_ENV);
+  console.log("test");
 
   return (
     <div className="mx-[5%] my-8 flex flex-col bg-off-white lg:mx-[10%] xl:mx-[15%]">
